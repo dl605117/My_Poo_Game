109bb280438e57bf5cdff7e5051ee84a945bc1a7
Darren <dl605117@gmail.com>
2018-06-21 15:00:43 -0700

    Added Functions - Length - Subtraction - Normalization

--- Engine/Vec2.cpp
+++ Engine/Vec2.cpp
@@ -1,4 +1,5 @@
 #include "Vec2.h"
+#include <cmath>
 
 Vec2::Vec2(float x, float y)
 {
@@ -24,5 +25,38 @@ Vec2 Vec2::operator*(const float scalar) const
 Vec2 & Vec2::operator*=(const float scalar)
 {
 	return *this = *this * scalar;
+}
+
+Vec2 Vec2::operator-(const Vec2& rhs) const
+{
+	return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2& Vec2::operator-=(const Vec2& rhs)
+{
+	return *this = *this - rhs;
+}
+
+float Vec2::GetLength() const
+{
+	return x * x + y * y;
+}
+
+float Vec2::GetLengthSq() const
+{
+	return std::sqrt(GetLength());
+}
+
+Vec2 & Vec2::Normalize()
+{
+	return *this = GetNormalized();
+}
+
+Vec2 Vec2::GetNormalized() const
+{
+	const float length = GetLength();
+	if (length != 0.0f)
+		return *this * (1 / length);
+	return *this;
 }
 
--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -9,6 +9,12 @@ public:
 	Vec2& operator+=(const Vec2& rhs);
 	Vec2 operator*(const float scalar) const;
 	Vec2& operator*=(const float scalar);
+	Vec2 operator-(const Vec2& rhs) const;
+	Vec2& operator-=(const Vec2& rhs);
+	float GetLength() const;
+	float GetLengthSq() const;
+	Vec2& Normalize();
+	Vec2 GetNormalized() const;
 public:
 	float x;
 	float y;
